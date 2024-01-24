eff45154342d12489b54fa4cb265f70fbaaa3a0b
paolo <paolo@LAPTOP-PFR3VSID>
2024-01-24 18:19:20 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Cane
+    {
+        public string Razza { get; set; }
+        public string Nome { get; set; }
+        public Persona Proprietario { get; set; }
+        public Cane (string Razza, string Nome)
+        {
+            this.Razza = Razza;
+            this.Nome = Nome;
+            this .Proprietario = new Persona ();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane cane = new Cane (Razza, Nome, Proprietario);
+            cane.Proprietario.Nome = "simone";
+            cane.Razza = "Labrador";
+            cane.Nome = "Ringhio";
+        }
+    }
+}
